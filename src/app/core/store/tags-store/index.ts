a8b7b7bdc9e6ac2e4c8505d82c39fcbd3e442df6
BuildTools <unconfigured@null.spigotmc.org>
2019-01-16 20:19:19 +0100

    Removed effect

--- src/app/core/store/tags-store/index.ts
+++ src/app/core/store/tags-store/index.ts
@@ -4,4 +4,3 @@ import * as TagsStoreSelectors from './selectors';
 
 export { TagsStoreModule } from './tags-store.module';
 export { TagsStoreActions, TagsStoreState, TagsStoreSelectors };
-export { TagsStoreEffects } from './effects';
